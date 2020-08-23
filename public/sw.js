fdb074f9f312877259609814040c032b496480b5
Abdurrahman Asad <bsef17m005@pucit.edu.pk>
2020-08-24 01:27:29 +0500

    ..

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,8 @@ var CACHE_NAME = 'quiz-app-cache-v1';
 
 var urlsToCache = [
   '/static/js/2.1a6a8bbd.chunk.js',
+  '/static/css/main.d4ba1882.chunk.css',
   'static/js/main.973440a5.chunk.js',
-  '/static/css/main.71772704.chunk.css',
   '/offline.html',
   "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple",
   "https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple",
